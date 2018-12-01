a6802e7f3c0e58be5d3ba64e0d008ed0048d4c06
Jeremy <jeremy@getcontrol.co>
2018-12-01 15:00:10 -0800

    Code to get data from click

--- eventPage.js
+++ eventPage.js
@@ -4,20 +4,12 @@ var menuItem = {
     "contexts": ["image"]
 };
 
-function isInt(value) {
-  return !isNaN(value) &&
-         parseInt(Number(value)) == value &&
-         !isNaN(parseInt(value, 10));
-}
-
 chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
     if (clickData.menuItemId == "poem"){
-        console.log(clickData)
-    }
-});
+        theImage = clickData.srcUrl
+        chrome.extension.getBackgroundPage().console.log(clickData);
 
-chrome.storage.onChanged.addListener(function(changes, storageName){
-    chrome.browserAction.setBadgeText({"text": changes.total.newValue.toString()});
+    }
 });
