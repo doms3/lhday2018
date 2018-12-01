13289333fb5065d1a07aa292ff513bc844821a70
Jeremy <jeremy@getcontrol.co>
2018-12-01 14:15:37 -0800

    event page addition

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+  let changeColor = document.getElementById('changeColor');
+
+  chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+  });
