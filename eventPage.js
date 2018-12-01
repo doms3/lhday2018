822feccb5043dba8b9d5461564b380224e48371e
Jeremy <jeremy@getcontrol.co>
2018-12-01 14:41:27 -0800

    This

--- eventPage.js
+++ eventPage.js
@@ -1,6 +1,23 @@
-var contextMenuItem = {
+var menuItem = {
     "id": "poem",
-    "title": "Poetica"
-    "contexts": ["selection"]
+    "title": "Create Poem",
+    "contexts": ["image"]
 };
-chrome.contextMenus.create(contextMenuItem);
+
+function isInt(value) {
+  return !isNaN(value) &&
+         parseInt(Number(value)) == value &&
+         !isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "poem"){
+        console.log(clickData)
+    }
+});
+
+chrome.storage.onChanged.addListener(function(changes, storageName){
+    chrome.browserAction.setBadgeText({"text": changes.total.newValue.toString()});
+});
