aad54efac44003b16d281f16ca4f392661609510
vtotient <quique.sira@gmail.com>
2018-12-01 17:59:00 -0800

    Putting everything together

--- content.js
+++ content.js
@@ -9,21 +9,48 @@ chrome.runtime.onMessage.addListener(
 		if( request == "initiateAPICall" ) {
 			var elem =  $( element );
 			var imgSrc = elem.src;
-			setAPICall(imgSrc);
+			setAPICall( imgSrc ).then( 
+                ( json ) => {
+                    console.log( json );
+                    changePicture( elem, json.raw ); 
+                });
 		}
 	}
 )
 
-setAPICall  = function(imageURL){
+function setAPICall( imageURL ) {
     return fetch(
-        "http://localhost:5000/",
+        "http://localhost:5000/poetica",
+        
         {
-            method: 'POST'
-            headers: new Headers({
+            method: 'POST',
+
+            headers: {
                 "Content-Type": "application/json"
-            }),
+            },
+
             body: {
                 imageURL: imageURL
             }
-        })
+        }).then( ( res ) => res.json() );
+}
+ 
+function changePicture( domElement, text ) {
+    var wrapper = $( "<div class='poetica-container' style='position: relative'></div>" );
+    domElement.wrap( wrapper );
+    var tmp = $( "<div class=\"overlay\"><div class=\"text\">" + text + "</div></div>" );
+    tmp.css( {
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: domElement.height(),
+        width: domElement.width(),
+        opacity: 0,
+        "background-color": "#111111"
+    } );
+    tmp.mouseenter( function() { $( this ).fadeTo( "slow", 0.80 ) } );
+    tmp.mouseleave( function() { $( this ).fadeTo( "fast", 0 ) } );
+    $( ".poetica-container" ).append( tmp );
 }
