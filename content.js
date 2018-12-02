7a8c77b99feddb0fd129acb1502417dede11d287
Jeremy <jeremy@getcontrol.co>
2018-12-01 16:52:08 -0800

    Call to API code

--- content.js
+++ content.js
@@ -4,11 +4,26 @@ document.addEventListener( "contextmenu", function( event ) {
 	element = event.target;
 }, true )
 
-chrome.runtime.onMessage.addListener( 
+chrome.runtime.onMessage.addListener(
 	function( request, sender, sendResponse ) {
 		if( request == "initiateAPICall" ) {
 			var elem =  $( element );
-			var imgSrc = elem.src; 
+			var imgSrc = elem.src;
+			setAPICall(imgSrc);
 		}
 	}
 )
+
+setAPICall  = function(imageURL){
+    return fetch(
+        "http://localhost:5000/",
+        {
+            method: 'POST'
+            headers: new Headers({
+                "Content-Type": "application/json"
+            }),
+            body: {
+                imageURL: imageURL
+            }
+        })
+}
